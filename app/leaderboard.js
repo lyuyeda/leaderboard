03fb4994a1736401f0e3daecc022488ac4cd19a6
lyuyeda <lyuyeda@gmail.com>
2017-10-10 10:07:09 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+PlayersList = new Mongo.Collection('players');
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    }
+  });
+}
+if(Meteor.isServer){
+  console.log("Hello server");
+}
